dd85e05b98d1658d0e6528139e228373216f5e88
Nathan <chaignonnathan@gmail.com>
2026-09-18 12:51:12 +0200

    debut etape 1

---
+++ JS/script.js
@@ -0,0 +1,44 @@
+const board = document.getElementById("game-board");
+const movesDisplay = document.getElementById("moves");
+const timerDisplay = document.getElementById("timer");
+const resultDisplay = document.getElementById("result");
+const restartBtn = document.getElementById("restart-btn");
+
+let dimension = 150;
+let imgStart = Math.floor(Math.random() * 100 + 1);
+
+
+let cards = [];
+let firstCard = null;
+let secondCard = null;
+let lockBoard = false;
+let moves = 0;
+let matchedCount = 0;
+let seconds = 0;
+let timerInterval = null;
+
+const images = [];
+for (let i = imgStart; i <= imgStart + 7; i++) {
+  images.push(`https://picsum.photos/seed/${i}/${dimension}/${dimension}`);
+}
+
+let cards = [...images, ...images];
+
+
+function shuffle(array) {
+  for (let i = array.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [array[i], array[j]] = [array[j], array[i]]; // On échange les deux places
+    /*cette syntaxe évite de passer obligatoirement par une variable temporaire*/
+  }
+}
+
+/*function shuffle1(array){
+  for (let i = 0; i < array.length; i++) {
+  const j = Math.floor(Math.random() * (i + 1));
+
+
+  
+  }
+} */
+
